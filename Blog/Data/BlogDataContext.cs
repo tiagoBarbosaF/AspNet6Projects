038f6a82f29c3914d5e780205e9d8a7fb70fbb5b
Tiago Barbosa <tiago@farias.cc>
2022-03-13 13:23:44 -0300

    Models, mappings and data access created

---
+++ Blog/Data/BlogDataContext.cs
@@ -0,0 +1,26 @@
+using Blog.Data.Mappings;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer(
+                "Server=localhost,1433;Database=Blog;User ID=sa;Password=S4k0r400;TrustServerCertificate=True ");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
+    }
+}
